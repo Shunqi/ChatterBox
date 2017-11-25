40ed7071c1bcc70d0bec936d7958ee99dc3bcb12
Shunqi <wangshunqi2@gmail.com>
2017-11-26 04:37:41 +0800

    Complete Task 1.1 - 1.5
    Implement session and user info management

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// Database
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/assignment2');
+
+// Add chats router
+var chats = require('./routes/chats');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +27,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// Make our db accessible to our router
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
+
+// all requests will go to chats router
+app.use('/', chats);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/chats.js
@@ -0,0 +1,122 @@
+var express = require('express');
+var router = express.Router();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+router.use(session({ secret: 'sqwang' }));
+router.use(bodyParser.json());
+
+/*
+* GET session use userId
+* if exists, return JSON
+* else return empty
+*/
+router.get('/load', function (req, res) {
+    if (req.session.userId) {
+        var db = req.db;
+        var collection = db.get('userList');
+        collection.find({ "_id": req.session.userId }, {}, function (err, docs) {
+            if (err === null)
+                res.json(docs);
+            else res.send({ msg: err });
+        });
+    } else {
+        res.send({ msg: 'no userId in session' });
+    }
+});
+
+
+/*
+* POST login
+* 
+*/
+router.post('/login', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    // check input values
+    if (!username || !password) {
+        res.send({ msg: "Null or invalid inputs", "body": req.body });
+    }
+    var db = req.db;
+    var collection = db.get('userList');
+    collection.findOne({ "name": username, "password": password }, { fields: { "_id": 1, "name": 1 } }, function (err, docs) {
+        if (err === null) {
+            if (docs.length === 0) {
+                res.send({ msg: "Login Failed", "username": username, "password": password });
+            } else {
+                req.session.userId = docs._id;
+                res.send({ msg: docs.name + " " + docs._id, "docs": docs });
+            }
+        } else {
+            res.send({ msg: "db error" + err });
+        }
+    });
+});
+
+/*
+* GET logout
+* 
+*/
+router.get('/logout', function (req, res) {
+    req.session.userId = null;
+    res.send({ msg: "" });
+});
+
+/*
+* GET get user info
+* 
+*/
+router.get('/getuserinfo', function (req, res) {
+    var userId = req.session.userId;
+    if (!userId) {
+        res.send({ msg: "Please login" });
+    }
+    var db = req.db;
+    var collection = db.get('userList');
+    collection.findOne({ "_id": userId }, { fields: { "mobileNumber": 1, "homeNumber": 1, "address": 1 } }, function (err, docs) {
+        if (err === null) {
+            if (docs.length === 0) {
+                res.send({ msg: "No such user" });
+            } else {
+                res.json(docs);
+            }
+        } else {
+            res.send({ msg: "db error" + err });
+        }
+    });
+});
+
+/*
+* PUT update user info
+* 
+*/
+router.put('/saveuserinfo', function (req, res) {
+    var mobileNumber = req.body.mobileNumber;
+    var homeNumber = req.body.homeNumber;
+    var address = req.body.address;
+    var userId = req.session.userId;
+    if (!userId) {
+        res.send({ msg: "Please login" });
+    }
+    var db = req.db;
+    var collection = db.get('userList');
+    collection.update({ "_id": userId}, { $set: { "mobileNumber": mobileNumber, "homeNumber": homeNumber, "address": address } }, function (err, result) {
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+// test connection, show all db data
+router.get('/test', function (req, res) {
+    var db = req.db;
+    var collection = db.get('userList');
+    collection.find({}, {}, function (err, docs) {
+        if (err === null)
+            res.json(docs);
+        else res.send({ msg: err });
+    });
+});
+
+
+module.exports = router;
