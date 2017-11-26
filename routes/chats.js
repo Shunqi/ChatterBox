6ab7daad4852a396d5cecb344976c0f506bc7ff6
Shunqi <wangshunqi2@gmail.com>
2017-11-27 02:41:22 +0800

    Implement Task 1-6
    Implement prototype of Task 1-6

--- routes/chats.js
+++ routes/chats.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb');
 
 router.use(session({ secret: 'sqwang' }));
 router.use(bodyParser.json());
@@ -100,10 +101,65 @@ router.put('/saveuserinfo', function (req, res) {
     }
     var db = req.db;
     var collection = db.get('userList');
-    collection.update({ "_id": userId}, { $set: { "mobileNumber": mobileNumber, "homeNumber": homeNumber, "address": address } }, function (err, result) {
+    collection.update({ "_id": userId }, { $set: { "mobileNumber": mobileNumber, "homeNumber": homeNumber, "address": address } }, function (err, result) {
         res.send(
             (err === null) ? { msg: '' } : { msg: err }
         );
+    });
+});
+
+
+/*
+* GET get conversation
+* 
+*/
+router.get('/getconversation/:friendid', function (req, res) {
+    var userId = req.session.userId;
+    if (!userId) {
+        res.send({ msg: "Please login" });
+    }
+    var db = req.db;
+    var collectionUserList = db.get('userList');
+    var friendid = req.params.friendid;
+    collectionUserList.findOne({ "_id": friendid }, { fields: { "name": 1, "icon": 1, "status": 1 } }, function (err, docs) {
+        if (err === null) {
+            if (!docs || docs.length === 0) {
+                res.send({ msg: "No such user" });
+            } else {
+                var friendName = docs.name;
+                console.log(friendName);
+                response = {
+                    icon: docs.icon,
+                    status: docs.icon
+                };
+
+
+                // check friendship
+                collectionUserList.findOne({ "_id": userId, "friends": { $elemMatch: { "name": friendName } } }, {}, function (err, docs) {
+                    if (err === null) {
+                        if (docs.length === 0) {
+                            res.send({ msg: "No such user" });
+                        } else {
+                            // retrive message
+                            var collectionMessageList = db.get('messageList');
+                            console.log(userId + friendid);
+                            collectionMessageList.find({ $or: [{ "senderId": userId, "receiverId": friendid }, { "senderId": friendid, "receiverId": userId }] }, function (err, docs) {
+                                if (err === null) {
+                                    response["messages"] = docs;
+                                    res.json(response);
+                                } else {
+                                    res.send({ msg: "db error" + err });
+                                }
+                            });
+                        }
+                    } else {
+                        res.send({ msg: "db error" + err });
+                    }
+                });
+            }
+        } else {
+            res.send({ msg: "db error" + err });
+        }
     });
 });
 
